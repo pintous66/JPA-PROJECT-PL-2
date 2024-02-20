37edf3687802b0f3baaad9d9b03c69068efed884
1221171isep <1221171@isep.ipp.pt>
2024-02-20 16:46:34 +0000

    Comentários, descrição dos métodos da classe Automovel.

--- src/main/java/org/domain/Automovel.java
+++ src/main/java/org/domain/Automovel.java
@@ -3,11 +3,25 @@ package org.domain;
 import java.util.Objects;
 
 public class Automovel {
+    /*
+    * kms do automovel
+    * não podem ser inferiores a zero, nem pode passar de um valor superior para um inferior
+     */
     public int kms;
 
+    /*
+    * Por omissão um carro começa com 0 km
+     */
     private int KMS_BY_OMISSION = 0;
 
+    /*
+    * Matricula do automovel não pode ser nula nem vazia, o resto depende de países e de regras de cada país
+     */
     private final String matricula;
+
+    /*
+    * Construtor de Automovel, valida a matricula se é nula ou vazia, lança uma excepção se for o caso
+     */
 
     public Automovel(String matricula){
         if(  matricula == null ||  matricula.length() == 0 ){
@@ -18,9 +32,15 @@ public class Automovel {
     }
 
 
+    /*
+    * Método que retorna a matricula do automovel
+     */
     public String getMatricula() {
         return matricula;
     }
+    /*
+    * Método que altera os kms do automovel, valida se o novo valor é superior ao actual, se for altera o valore e retorna true, se não retorna falso
+     */
     public boolean alterarKms(int kmNovo){
         if(kmNovo > kms){
             kms = kmNovo;
@@ -31,6 +51,9 @@ public class Automovel {
 
     }
 
+    /*
+    * Método que retorna a representação textual do automovel
+     */
     @Override
     public String toString() {
         String s = "Matricula = " + matricula + " Kms = " + kms;
