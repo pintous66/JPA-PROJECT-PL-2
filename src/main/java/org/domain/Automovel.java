5461f920bdc439369accbbdf42ac35f67d471a3c
GabrielR12 <1221162@isep.ipp.pt>
2024-02-20 15:27:00 +0000

    Implementação do método alterarClasse da classe GrupoAutomovel.

--- src/main/java/org/domain/Automovel.java
+++ src/main/java/org/domain/Automovel.java
@@ -1,5 +1,4 @@
 package org.domain;
 
 public class Automovel {
-
 }
--- src/main/java/org/domain/GrupoAutomovel.java
+++ src/main/java/org/domain/GrupoAutomovel.java
@@ -1,5 +1,9 @@
 package org.domain;
 
 public class GrupoAutomovel {
+    private String classe;
 
+    public void alterarClasse(String novaClasse){
+        classe = novaClasse;
+    }
 }
