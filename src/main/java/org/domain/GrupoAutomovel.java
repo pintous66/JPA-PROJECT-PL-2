f8e3946e09a766139ad67a59338285d1709f5f74
1221171isep <1221171@isep.ipp.pt>
2024-02-20 15:23:42 +0000

    Criar metodo alterarNumeroPortas da classe GrupoAutomovel

--- src/main/java/org/domain/GrupoAutomovel.java
+++ src/main/java/org/domain/GrupoAutomovel.java
@@ -2,4 +2,9 @@ package org.domain;
 
 public class GrupoAutomovel {
 
+    private int numeroPortas;
+
+    public void alterarNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
 }
